cf06d90feec5fed4924399d539d84fbb104a05fe
nick <noemail4u@Noemail.nope>
2020-01-28 21:29:12 +0000

    Get things working.

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const net = require('net');
 const fs = require('fs');
 const honeyPorts = [3389,445,139,23,3306];
 const output = 'output.log';
-const ips = new Set();
+let ips = new Set();
+try{ 
+	ips = new Set(fs.readFileSync('output.log').toString().split('\n'));
+}catch(e){
+
+}
 
 const servers = honeyPorts.map(port=>{
 	return net.createServer(socket=>{
 		socket.on('data',data=>{
-			ips.add(socket.address().address);
-			console.log(socket.address().address);
+			const address = socket.remoteAddress.replace('::ffff:','');
+			ips.add(address);
+			console.log(address);
+			socket.on('error',e=>{});
 			//Sling their bullshit back at em
-			socket.write(data);
+			try{
+				socket.write(data);
+			}catch(e){}
 		});
-	}).listen(port);
+	}).listen(port,()=>{
+		console.log(`Listening on port ${port}`);
+	});
 });
 setInterval(()=>{
 	fs.writeFileSync(output,[...ips].join('\n'));
